e0bf1c61f13288d514e9c0de977a1d56757d2a0d
ratina0 <ratina0@gmail.com>
2023-09-12 20:46:30 +0200

    changed titles

--- src/config.mjs
+++ src/config.mjs
@@ -1,13 +1,13 @@
 import defaultImage from './assets/images/default.png';
 
 const CONFIG = {
-  name: 'Martin',
+  name: 'Home',
 
   origin: 'https://ratinfx.github.com',
   basePathname: '/',
   trailingSlash: false,
 
-  title: "Martin's Portfolio",
+  title: 'Portfolio',
   description: 'Software developer, beginner animator and YouTube creator',
   defaultImage: defaultImage,
 
