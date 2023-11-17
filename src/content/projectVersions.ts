960d8aa78e935435b172294b67ac8f6dc9912fb4
ratina0 <ratina0@gmail.com>
2023-11-17 17:21:24 +0100

    Refactor github links + vpflow 1.4.7 release

--- src/content/projectVersions.ts
+++ src/content/projectVersions.ts
@@ -1,4 +1,11 @@
 import type { ProjectVersion } from '~/types';
+import {
+  betterSearchLink,
+  customFadesLink,
+  shortenExtendMediaLink,
+  vpdata as vpdataLink,
+  vpflowLink,
+} from '~/utils/projectinfo';
 
 const betterSearch: ProjectVersion = {
   project: 'better-search',
@@ -6,28 +13,28 @@ const betterSearch: ProjectVersion = {
     {
       release: '1.4.3',
       releaseDate: new Date('2023-07-14'),
-      vp13exe: 'https://github.com/RatinFX/BetterSearch/releases/download/1.4.3/BetterSearch13-1.4.3.exe',
-      vp14exe: 'https://github.com/RatinFX/BetterSearch/releases/download/1.4.3/BetterSearch14-1.4.3.exe',
-      vp13zip: 'https://github.com/RatinFX/BetterSearch/releases/download/1.4.3/BetterSearch13-1.4.3.zip',
-      vp14zip: 'https://github.com/RatinFX/BetterSearch/releases/download/1.4.3/BetterSearch14-1.4.3.zip',
+      vp13exe: betterSearchLink('1.4.3', '13', 'exe'),
+      vp14exe: betterSearchLink('1.4.3', '14', 'exe'),
+      vp13zip: betterSearchLink('1.4.3', '13', 'zip'),
+      vp14zip: betterSearchLink('1.4.3', '14', 'zip'),
       changes: ['Fixed crashes on Vegas Pro 20.0 when the extension threw an error', 'Vegas Pro Data update'],
     },
     {
       release: '1.4.2',
       releaseDate: new Date('2023-05-15'),
-      vp13exe: 'https://github.com/RatinFX/BetterSearch/releases/download/1.4.2/BetterSearch13-1.4.2.exe',
-      vp14exe: 'https://github.com/RatinFX/BetterSearch/releases/download/1.4.2/BetterSearch14-1.4.2.exe',
-      vp13zip: 'https://github.com/RatinFX/BetterSearch/releases/download/1.4.2/BetterSearch13-1.4.2.zip',
-      vp14zip: 'https://github.com/RatinFX/BetterSearch/releases/download/1.4.2/BetterSearch14-1.4.2.zip',
+      vp13exe: betterSearchLink('1.4.2', '13', 'exe'),
+      vp14exe: betterSearchLink('1.4.2', '14', 'exe'),
+      vp13zip: betterSearchLink('1.4.2', '13', 'zip'),
+      vp14zip: betterSearchLink('1.4.2', '14', 'zip'),
       changes: ['Move from <b>Views</b> to <b>Tools - Extensions</b>'],
     },
     {
       release: '1.4.1',
       releaseDate: new Date('2023-05-14'),
-      vp13exe: 'https://github.com/RatinFX/BetterSearch/releases/download/1.4.1/BetterSearch13-1.4.1.exe',
-      vp14exe: 'https://github.com/RatinFX/BetterSearch/releases/download/1.4.1/BetterSearch14-1.4.1.exe',
-      vp13zip: 'https://github.com/RatinFX/BetterSearch/releases/download/1.4.1/BetterSearch13-1.4.1.zip',
-      vp14zip: 'https://github.com/RatinFX/BetterSearch/releases/download/1.4.1/BetterSearch14-1.4.1.zip',
+      vp13exe: betterSearchLink('1.4.1', '13', 'exe'),
+      vp14exe: betterSearchLink('1.4.1', '14', 'exe'),
+      vp13zip: betterSearchLink('1.4.1', '13', 'zip'),
+      vp14zip: betterSearchLink('1.4.1', '14', 'zip'),
       changes: [
         'Vegas Pro Data update',
         'Moved from <b>Views</b> to <b>Tools - Extensions</b>',
@@ -38,15 +45,15 @@ const betterSearch: ProjectVersion = {
     {
       release: '1.3.0',
       releaseDate: new Date('2023-03-12'),
-      vp13zip: 'https://github.com/RatinFX/BetterSearch/releases/download/1.3.0/BetterSearch13-1.3.0.zip',
-      vp14zip: 'https://github.com/RatinFX/BetterSearch/releases/download/1.3.0/BetterSearch14-1.3.0.zip',
+      vp13zip: betterSearchLink('1.3.0', '13', 'zip'),
+      vp14zip: betterSearchLink('1.3.0', '14', 'zip'),
       changes: ['Rebranding', 'Added <b>Creator</b> and <b>About</b> tab to the top row', 'VegasProData update'],
     },
     {
       release: '1.2.1',
       releaseDate: new Date('2023-02-26'),
-      vp13zip: 'https://github.com/RatinFX/BetterSearch/releases/download/1.2.1/BetterSearch13-1.2.1.zip',
-      vp14zip: 'https://github.com/RatinFX/BetterSearch/releases/download/1.2.1/BetterSearch14-1.2.1.zip',
+      vp13zip: betterSearchLink('1.2.1', '13', 'zip'),
+      vp14zip: betterSearchLink('1.2.1', '14', 'zip'),
     },
   ],
 };
@@ -57,8 +64,8 @@ const customFades: ProjectVersion = {
     {
       release: '1.1.0',
       releaseDate: new Date('2023-03-11'),
-      vp13zip: 'https://github.com/RatinFX/CustomFades/releases/download/1.1.0/CustomFades13.dll',
-      vp14zip: 'https://github.com/RatinFX/CustomFades/releases/download/1.1.0/CustomFades14.dll',
+      vp13zip: customFadesLink('1.1.0', '13'),
+      vp14zip: customFadesLink('1.1.0', '14'),
       changes: [
         'Added top row with a <b>Creator</b> and <b>About</b> tab',
         'Removed <b>Help</b> button and moved information to <b>About</b> ',
@@ -68,8 +75,8 @@ const customFades: ProjectVersion = {
     {
       release: '1.0.0',
       releaseDate: new Date('2023-01-22'),
-      vp13zip: 'https://github.com/RatinFX/CustomFades/releases/download/1.0.0/CustomFades13.dll',
-      vp14zip: 'https://github.com/RatinFX/CustomFades/releases/download/1.0.0/CustomFades14.dll',
+      vp13zip: customFadesLink('1.0.0', '13'),
+      vp14zip: customFadesLink('1.0.0', '14'),
     },
   ],
 };
@@ -80,8 +87,8 @@ const shortenExtendMedia: ProjectVersion = {
     {
       release: '1.1.0',
       releaseDate: new Date('2023-03-12'),
-      vp13zip: 'https://github.com/RatinFX/ShortenExtendMedia/releases/download/1.1.0/ShortenExtendMedia13.dll',
-      vp14zip: 'https://github.com/RatinFX/ShortenExtendMedia/releases/download/1.1.0/ShortenExtendMedia14.dll',
+      vp13zip: shortenExtendMediaLink('1.1.0', '13'),
+      vp14zip: shortenExtendMediaLink('1.1.0', '14'),
       changes: [
         'Added top row with a <b>Creator</b> and <b>About</b> tab',
         'Removed <b>Help</b> button and moved information to <b>About</b> ',
@@ -90,8 +97,8 @@ const shortenExtendMedia: ProjectVersion = {
     {
       release: '1.0.0',
       releaseDate: new Date('2023-01-22'),
-      vp13zip: 'https://github.com/RatinFX/ShortenExtendMedia/releases/download/1.0.0/ShortenExtendMedia13.dll',
-      vp14zip: 'https://github.com/RatinFX/ShortenExtendMedia/releases/download/1.0.0/ShortenExtendMedia14.dll',
+      vp13zip: shortenExtendMediaLink('1.0.0', '13'),
+      vp14zip: shortenExtendMediaLink('1.0.0', '14'),
     },
   ],
 };
@@ -102,29 +109,29 @@ const vegasProData: ProjectVersion = {
     {
       release: '1.4.2',
       releaseDate: new Date('2023-07-14'),
-      vp13zip: 'https://github.com/RatinFX/VegasProData/releases/download/1.4.2/VegasProData13-1.4.2.zip',
-      vp14zip: 'https://github.com/RatinFX/VegasProData/releases/download/1.4.2/VegasProData14-1.4.2.zip',
+      vp13zip: vpdataLink('1.4.2', '13'),
+      vp14zip: vpdataLink('1.4.2', '14'),
       changes: ['Fixed crashes on Vegas Pro 20.0 when the extension threw an error'],
     },
     {
       release: '1.4.1',
       releaseDate: new Date('2023-05-14'),
-      vp13zip: 'https://github.com/RatinFX/VegasProData/releases/download/1.4.1/VegasProData13-1.4.1.zip',
-      vp14zip: 'https://github.com/RatinFX/VegasProData/releases/download/1.4.1/VegasProData14-1.4.1.zip',
+      vp13zip: vpdataLink('1.4.1', '13'),
+      vp14zip: vpdataLink('1.4.1', '14'),
       changes: ['VegasProFlow update'],
     },
     {
       release: '1.3.0',
       releaseDate: new Date('2023-03-12'),
-      vp13zip: 'https://github.com/RatinFX/VegasProData/releases/download/1.3.0/VegasProData13-1.3.0.zip',
-      vp14zip: 'https://github.com/RatinFX/VegasProData/releases/download/1.3.0/VegasProData14-1.3.0.zip',
+      vp13zip: vpdataLink('1.3.0', '13'),
+      vp14zip: vpdataLink('1.3.0', '14'),
       changes: ['Rebranding update', 'Added Custom Colors class', 'Added fileName to FavoriteConfig constructor'],
     },
     {
       release: '1.2.0',
       releaseDate: new Date('2023-02-18'),
-      vp13zip: 'https://github.com/RatinFX/VegasProData/releases/download/1.2.0/VegasProData13-1.2.0.zip',
-      vp14zip: 'https://github.com/RatinFX/VegasProData/releases/download/1.2.0/VegasProData14-1.2.0.zip',
+      vp13zip: vpdataLink('1.2.0', '13'),
+      vp14zip: vpdataLink('1.2.0', '14'),
     },
   ],
 };
@@ -133,32 +140,43 @@ const vegasProFlow: ProjectVersion = {
   project: 'vegas-pro-flow',
   versions: [
     {
+      release: '1.4.7',
+      releaseDate: new Date('2023-11-17'),
+      vp14exe: vpflowLink('1.4.7', '14', 'exe'),
+      vp14zip: vpflowLink('1.4.7', '14', 'zip'),
+      changes: [
+        'Fixed display issues with the yellow handler circle',
+        'Added version number to the Undocked Window',
+        '!! Only releasing for VP14 as I cannot test VP13 or below',
+      ],
+    },
+    {
       release: '1.4.6',
       releaseDate: new Date('2023-11-13'),
-      vp14exe: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.4.6/VegasProFlow14-1.4.6.exe',
-      vp14zip: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.4.6/VegasProFlow14-1.4.6.zip',
+      vp14exe: vpflowLink('1.4.6', '14', 'exe'),
+      vp14zip: vpflowLink('1.4.6', '14', 'zip'),
       changes: [
         'Fixed an issue with Pan/Crop where it threw and error when the Rotation Center did not change during the animation',
-        '!! Only releasing for VP14 as I cannot test VP13',
+        '!! Only releasing for VP14 as I cannot test VP13 or below',
       ],
     },
     {
       release: '1.4.5',
       releaseDate: new Date('2023-11-02'),
-      vp14exe: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.4.5/VegasProFlow14-1.4.5.exe',
-      vp14zip: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.4.5/VegasProFlow14-1.4.5.zip',
+      vp14exe: vpflowLink('1.4.5', '14', 'exe'),
+      vp14zip: vpflowLink('1.4.5', '14', 'zip'),
       changes: [
         'Fixed an issue with Pan/Crop where the Rotation Center was set to 0 during the animated frames regardless of its given value',
-        '!! Only releasing for VP14 as I cannot test VP13',
+        '!! Only releasing for VP14 as I cannot test VP13 or below',
       ],
     },
     {
       release: '1.4.4',
       releaseDate: new Date('2023-07-14'),
-      vp13exe: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.4.4/VegasProFlow13-1.4.4.exe',
-      vp14exe: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.4.4/VegasProFlow14-1.4.4.exe',
-      vp13zip: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.4.4/VegasProFlow13-1.4.4.zip',
-      vp14zip: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.4.4/VegasProFlow14-1.4.4.zip',
+      vp13exe: vpflowLink('1.4.4', '13', 'exe'),
+      vp14exe: vpflowLink('1.4.4', '14', 'exe'),
+      vp13zip: vpflowLink('1.4.4', '13', 'zip'),
+      vp14zip: vpflowLink('1.4.4', '14', 'zip'),
       changes: [
         'Fixed an issue with Pan/Crop where the Rotation was set to 0 during the animated frames regardless of its given value',
         'Fixed crashes on Vegas Pro 20.0 when the extension threw an error',
@@ -167,19 +185,19 @@ const vegasProFlow: ProjectVersion = {
     {
       release: '1.4.3',
       releaseDate: new Date('2023-05-15'),
-      vp13exe: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.4.3/VegasProFlow13-1.4.3.exe',
-      vp14exe: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.4.3/VegasProFlow14-1.4.3.exe',
-      vp13zip: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.4.3/VegasProFlow13-1.4.3.zip',
-      vp14zip: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.4.3/VegasProFlow14-1.4.3.zip',
+      vp13exe: vpflowLink('1.4.3', '13', 'exe'),
+      vp14exe: vpflowLink('1.4.3', '14', 'exe'),
+      vp13zip: vpflowLink('1.4.3', '13', 'zip'),
+      vp14zip: vpflowLink('1.4.3', '14', 'zip'),
       changes: ['Fixed more <b>Fade in/out Transition Progress Envelope</b> issues'],
     },
     {
       release: '1.4.2',
       releaseDate: new Date('2023-05-14'),
-      vp13exe: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.4.2/VegasProFlow13-1.4.2.exe',
-      vp14exe: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.4.2/VegasProFlow14-1.4.2.exe',
-      vp13zip: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.4.2/VegasProFlow13-1.4.2.zip',
-      vp14zip: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.4.2/VegasProFlow14-1.4.2.zip',
+      vp13exe: vpflowLink('1.4.2', '13', 'exe'),
+      vp14exe: vpflowLink('1.4.2', '14', 'exe'),
+      vp13zip: vpflowLink('1.4.2', '13', 'zip'),
+      vp14zip: vpflowLink('1.4.2', '14', 'zip'),
       changes: [
         'Removed <b>Mute</b> from supported Track Envelopes',
         'Fixed various <b>Fade in/out Transition Progress Envelope</b> issues',
@@ -188,10 +206,10 @@ const vegasProFlow: ProjectVersion = {
     {
       release: '1.4.1',
       releaseDate: new Date('2023-05-14'),
-      vp13exe: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.4.1/VegasProFlow13-1.4.1.exe',
-      vp14exe: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.4.1/VegasProFlow14-1.4.1.exe',
-      vp13zip: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.4.1/VegasProFlow13-1.4.1.zip',
-      vp14zip: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.4.1/VegasProFlow14-1.4.1.zip',
+      vp13exe: vpflowLink('1.4.1', '13', 'exe'),
+      vp14exe: vpflowLink('1.4.1', '14', 'exe'),
+      vp13zip: vpflowLink('1.4.1', '13', 'zip'),
+      vp14zip: vpflowLink('1.4.1', '14', 'zip'),
       changes: [
         'Changed versioning',
         'Moved from <b>Views</b> to <b>Tools - Extensions</b>',
@@ -206,8 +224,8 @@ const vegasProFlow: ProjectVersion = {
     {
       release: '1.0.3',
       releaseDate: new Date('2023-03-29'),
-      vp13zip: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.0.3/VegasProFlow13-1.0.3.zip',
-      vp14zip: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.0.3/VegasProFlow14-1.0.3.zip',
+      vp13zip: vpflowLink('1.0.3', '13', 'zip'),
+      vp14zip: vpflowLink('1.0.3', '14', 'zip'),
       changes: [
         'Fixed multiple issues with Adding new Custom Curves',
         'Added a warning for Track Motion animating, make sure you <b>close the window</b> before applying curves!',
@@ -217,8 +235,8 @@ const vegasProFlow: ProjectVersion = {
     {
       release: '1.0.2',
       releaseDate: new Date('2023-03-29'),
-      vp13zip: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.0.2/VegasProFlow13-1.0.2.zip',
-      vp14zip: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.0.2/VegasProFlow14-1.0.2.zip',
+      vp13zip: vpflowLink('1.0.2', '13', 'zip'),
+      vp14zip: vpflowLink('1.0.2', '14', 'zip'),
       changes: [
         'Added <b>Settings option</b> to toggle "Check for Updates on Start"',
         'Refactored the <b>About tab</b>',
@@ -227,8 +245,8 @@ const vegasProFlow: ProjectVersion = {
     {
       release: '1.0.1',
       releaseDate: new Date('2023-03-18'),
-      vp13zip: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.0.1/VegasProFlow13-1.0.1.zip',
-      vp14zip: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.0.1/VegasProFlow14-1.0.1.zip',
+      vp13zip: vpflowLink('1.0.1', '13', 'zip'),
+      vp14zip: vpflowLink('1.0.1', '14', 'zip'),
       changes: [
         'Added <b>Settings option</b> to toggle "Show Support view on Start"',
         'Extended the description on the About tab',
@@ -237,8 +255,8 @@ const vegasProFlow: ProjectVersion = {
     {
       release: '1.0.0',
       releaseDate: new Date('2023-03-16'),
-      vp13zip: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.0.0/VegasProFlow13-1.0.0.zip',
-      vp14zip: 'https://github.com/RatinFX/VegasProFlow/releases/download/1.0.0/VegasProFlow14-1.0.0.zip',
+      vp13zip: vpflowLink('1.0.0', '13', 'zip'),
+      vp14zip: vpflowLink('1.0.0', '14', 'zip'),
     },
   ],
 };
